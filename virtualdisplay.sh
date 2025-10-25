304c73c718bccd94fea62cae35448fc9a3aca86e
Maftei Albert-Alexandru <maftei.albert.alexandru@gmail.com>
2025-10-25 11:18:04 +0200

    Virtual display fix

--- virtualdisplay.sh
+++ virtualdisplay.sh
@@ -5,12 +5,15 @@
 #  so GUI apps (like turtlesim) can be viewed via port 6080.
 # ==========================================================
 
-#!/usr/bin/env bash
 # ==========================================================
 # Virtual Display Starter for ROS 2 Workshop
 # ==========================================================
 
 # stop any old session first
+
+export XDG_SESSION_TYPE=x11
+unset WAYLAND_DISPLAY
+
 echo "🧹 Checking for previous virtual display processes..."
 pkill Xvfb x11vnc websockify fluxbox 2>/dev/null || true
 sleep 1
@@ -18,14 +21,14 @@ sleep 1
 
 # Create virtual display
 export DISPLAY=:0
-Xvfb :0 -screen 0 1280x800x16 &
+Xvfb :0 -screen 0 1280x800x16 &> /dev/null &
 
 # Start lightweight window manager
 fluxbox &> /dev/null &
 
 # Start VNC + WebSocket bridge
-x11vnc -display :0 -forever -nopw -quiet -rfbport 5900 &> /dev/null &
-websockify --web=/usr/share/novnc/ 6080 localhost:5900 &> /dev/null &
+x11vnc -env FD_X11=1 -display :0 -forever -quiet -nopw -rfbport 5900 &> /dev/null &
+websockify --web=/usr/share/novnc/ 6080 localhost:5900 &> /dev/null  &
 
 echo ""
 echo "🖥️  Virtual desktop is now running!"
@@ -40,4 +43,4 @@ echo "To stop this display:  pkill Xvfb x11vnc websockify fluxbox"
 echo ""
 
 # Keep container alive
-tail -f /dev/null
+tail -f "/dev/null"
